538b625923be715f25576c840389bbf9bd4084c4
Shivam Sharma <85331338+ShivamSharma3589@users.noreply.github.com>
2021-12-04 02:17:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,27 @@ let circle = document.getElementsByClassName('circle');
 let checkedInput = document.querySelectorAll('input[type="checkbox"]');
 let totalInput = checkedInput.length;
 let checkedCount = 0;
+let storeData = [];
+
 function progressChecker() {
     checkedCount = 0;
     for (let i = 0; i < totalInput; i++) {
+        storeData[i] = false;
         if (checkedInput[i].checked) {
             checkedCount++;
+            storeData[i] = true;
         }
     }
+    document.getElementById('completed').innerHTML = checkedCount;
+    window.localStorage.setItem('key',JSON.stringify(storeData));
     return checkedCount;
 }
+
 function progress() {
     let counter = Math.floor((((checkedCount/totalInput)*360)/360) * 100);
     let cover = document.getElementById('cover');
     let val = (progressChecker()/totalInput)*360;
+    let status = document.getElementById('status');
     console.log(val);
     if(val <= 360)
         circle[0].style.transform = `rotate(${val}deg)`;
@@ -41,23 +49,85 @@ function progress() {
         if (percentage == counter){
             clearInterval(counterInterval);
         }
+        if (counter == 100) {
+            status.innerHTML = '--Tasks Completed--';
+        } else {
+            status.innerHTML = `In Progress . . .`;
+        }
     }, 50);
 
 }
+
 for (let i = 0; i < totalInput; i++) {
     checkedInput[i].addEventListener('click',progress);
 }
 
-// let val = 0;
-// setInterval(() => {
-//     if(val < 180)
-//         circle[1].style.transform = `rotate(${val++}deg)`;
-//     if(val > 180)
-//         circle[2].style.display = 'none';
-//     if(val < 360)
-//         circle[0].style.transform = `rotate(${val++}deg)`;
-// }, 100);
+window.addEventListener('load', ()=>{
+    document.getElementById('total').innerHTML = totalInput;
+    let records = window.localStorage.getItem('key');
+    if (records != null) {
+        storeData = JSON.parse(records);
+        for (let i = 0; i < totalInput; i++) {
+            checkedInput[i].checked = storeData[i];
+        }
+        progress();
+    }
+})
 
+// function fileManagement() {//retrieves items in the localStorage
+//         console.log("retrieve records");
+//         let records = window.localStorage.getItem('key');
+//         if (records == null) {
+            
+//         }
+// }
+
+/*
+function store(){ //stores items in the localStorage
+    var brand = document.getElementById('carBrand').value;
+    var price = document.getElementById('carPrice').value;
+    var key = document.getElementById('key').value; //gets the key from the user
+
+    const car = {
+        brand: brand,
+        price: price,
+    }
+
+    window.localStorage.setItem(key,JSON.stringify(car));  
+    //converting object to string
+}
+
+function retrieveRecords(){ //retrieves items in the localStorage
+    console.log("retrieve records");
+    var key = document.getElementById('retrieveKey').value;
+    var records = window.localStorage.getItem(key);
+    var paragraph = document.createElement("p");
+    var infor = document.createTextNode(records);
+    paragraph.appendChild(infor);
+    var element = document.getElementById("retrieve");
+    element.appendChild(paragraph);
+}
+
+function removeItem(){  //deletes item from localStorage
+    var key = document.getElementById('removeKey').value;
+    localStorage.removeItem(key)
+    console.log("remove items");
+}
+function clearStorage(){ 
+    //clears the entire localStorage
+    localStorage.clear()
+    console.log("clear records");
+}
+window.onload =function(){ //ensures the page is loaded before functions are executed.
+    document.getElementById("carForm").onsubmit = store
+    document.getElementById("clearButton").onclick = clearStorage
+    document.getElementById("removeButton").onclick = removeItem
+    document.getElementById("retrieveButton").onclick = retrieveRecords
+}
+*/
+
+
+/*
 // Get the root element
 let root = document.querySelector(':root');
 
@@ -74,3 +144,4 @@ function myFunction_set() {
   // Set the value of variable --blue to another value (in this case "lightblue")
   root.style.setProperty('--blue', 'lightblue');
 }
+*/
