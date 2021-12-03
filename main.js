c8386adae348cf559ad23ead9bc54f9aaa255b42
Shivam Sharma <85331338+ShivamSharma3589@users.noreply.github.com>
2021-12-04 03:50:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,147 +1,88 @@
-// main js goes here 
-let circle = document.getElementsByClassName('circle');
-let checkedInput = document.querySelectorAll('input[type="checkbox"]');
-let totalInput = checkedInput.length;
-let checkedCount = 0;
-let storeData = [];
+let circle = document.getElementsByClassName('circle'); //circle class divs
+let checkedInput = document.querySelectorAll('input[type="checkbox"]'); //all input[checkbox]
+let totalInput = checkedInput.length;   //counting the number of input
+let checkedCount = 0;   //counter variable for sequence checking 
+let storeData = [];     //creating array to store data locally
 
+// function check the number of input checked 
 function progressChecker() {
-    checkedCount = 0;
+    checkedCount = 0;   //reset to 0 first then starts count
     for (let i = 0; i < totalInput; i++) {
-        storeData[i] = false;
+        storeData[i] = false;   //presuming the value to be false 
         if (checkedInput[i].checked) {
-            checkedCount++;
-            storeData[i] = true;
+            checkedCount++;     //increasing the counter by 1
+            storeData[i] = true;    //setting the value true along 
         }
     }
-    document.getElementById('completed').innerHTML = checkedCount;
-    window.localStorage.setItem('key',JSON.stringify(storeData));
+    document.getElementById('completed').innerHTML = checkedCount;  //printing the number of task completed
+    window.localStorage.setItem('key', JSON.stringify(storeData));  //storing the data locally in the form of string
     return checkedCount;
 }
 
+// function manage the rounded progress bar and percentage value of cover section 
 function progress() {
-    let counter = Math.floor((((checkedCount/totalInput)*360)/360) * 100);
+    // to claculate the percentage 
+    let counter = Math.floor((((checkedCount / totalInput) * 360) / 360) * 100);
     let cover = document.getElementById('cover');
-    let val = (progressChecker()/totalInput)*360;
-    let status = document.getElementById('status');
-    console.log(val);
-    if(val <= 360)
-        circle[0].style.transform = `rotate(${val}deg)`;
-    if(val < 180){
-        circle[1].style.transform = `rotate(${val}deg)`;
-    }
-    else{
-        circle[1].style.transform = `rotate(180deg)`;
-    }
-    if(val >= 180)
-        circle[2].style.opacity = '0';
-    else
-        circle[2].style.opacity = '1';
+    let val = (progressChecker() / totalInput) * 360;   //to calculate the degree
+    let status = document.getElementById('status');     //for transformation of sub-heading
+    let percentage = Math.floor((val / 360) * 100);     //now converting the degree into practical
+    cover.innerHTML = counter;  //changing the value of cover
 
-    let percentage = Math.floor((val/360) * 100);
-    cover.innerHTML = counter;
+    // creating set interval section 
     let counterInterval = setInterval(() => {
+
+        // section for counting animation 
+        // for increasing animation 
         if (percentage > counter) {
             cover.innerHTML = `${++counter}%`;
-        } else if (percentage < counter) {
-            cover.innerHTML = `${--counter}%`;            
+        } else if (percentage < counter) {  //for decreasing animation 
+            cover.innerHTML = `${--counter}%`;
         }
-        if (percentage == counter){
-            clearInterval(counterInterval);
+        if (percentage == counter) {
+            clearInterval(counterInterval);   //for stoping the interval
         }
+        // for changing the sub heading 
         if (counter == 100) {
             status.innerHTML = '--Tasks Completed--';
         } else {
             status.innerHTML = `In Progress . . .`;
         }
-    }, 50);
 
-}
-
-for (let i = 0; i < totalInput; i++) {
-    checkedInput[i].addEventListener('click',progress);
-}
-
-window.addEventListener('load', ()=>{
-    document.getElementById('total').innerHTML = totalInput;
-    let records = window.localStorage.getItem('key');
-    if (records != null) {
-        storeData = JSON.parse(records);
-        for (let i = 0; i < totalInput; i++) {
-            checkedInput[i].checked = storeData[i];
+        // section for rotation of progress bar 
+        let shortValue = counter * 360 / 100;
+        // for first circle 
+        if (shortValue <= 360)
+            circle[0].style.transform = `rotate(${shortValue}deg)`;
+        // for second circle 
+        if (shortValue < 180) {
+            circle[1].style.transform = `rotate(${shortValue}deg)`;
         }
-        progress();
+        else {
+            circle[1].style.transform = `rotate(180deg)`;
+        }
+        // for third circle 
+        if (shortValue >= 180)
+            circle[2].style.opacity = '0';
+        else
+            circle[2].style.opacity = '1';
+    }, 25);
+
+}
+
+// to trigger the click event on every input  
+for (let i = 0; i < totalInput; i++) {
+    checkedInput[i].addEventListener('click', progress);
+}
+
+window.addEventListener('load', () => {
+    document.getElementById('total').innerHTML = totalInput;    //onload show total tasks
+    let records = window.localStorage.getItem('key');       //for retreive the data 
+    if (records != null) {  //in case no previous data maintained
+        storeData = JSON.parse(records);    //retrieving the arrays
+        for (let i = 0; i < totalInput; i++) {
+            checkedInput[i].checked = storeData[i]; //now loading the values 
+        }
+        progress();     //clling the function progress for touch up 
     }
 })
-
-// function fileManagement() {//retrieves items in the localStorage
-//         console.log("retrieve records");
-//         let records = window.localStorage.getItem('key');
-//         if (records == null) {
-            
-//         }
-// }
-
-/*
-function store(){ //stores items in the localStorage
-    var brand = document.getElementById('carBrand').value;
-    var price = document.getElementById('carPrice').value;
-    var key = document.getElementById('key').value; //gets the key from the user
-
-    const car = {
-        brand: brand,
-        price: price,
-    }
-
-    window.localStorage.setItem(key,JSON.stringify(car));  
-    //converting object to string
-}
-
-function retrieveRecords(){ //retrieves items in the localStorage
-    console.log("retrieve records");
-    var key = document.getElementById('retrieveKey').value;
-    var records = window.localStorage.getItem(key);
-    var paragraph = document.createElement("p");
-    var infor = document.createTextNode(records);
-    paragraph.appendChild(infor);
-    var element = document.getElementById("retrieve");
-    element.appendChild(paragraph);
-}
-
-function removeItem(){  //deletes item from localStorage
-    var key = document.getElementById('removeKey').value;
-    localStorage.removeItem(key)
-    console.log("remove items");
-}
-function clearStorage(){ 
-    //clears the entire localStorage
-    localStorage.clear()
-    console.log("clear records");
-}
-window.onload =function(){ //ensures the page is loaded before functions are executed.
-    document.getElementById("carForm").onsubmit = store
-    document.getElementById("clearButton").onclick = clearStorage
-    document.getElementById("removeButton").onclick = removeItem
-    document.getElementById("retrieveButton").onclick = retrieveRecords
-}
-*/
-
-
-/*
-// Get the root element
-let root = document.querySelector(':root');
-
-// Create a function for getting a variable value
-function myFunction_get() {
-  // Get the styles (properties and values) for the root
-  let rootStyle = getComputedStyle(root);
-  // Alert the value of the --blue variable
-  alert("The value of --blue is: " + rootStyle.getPropertyValue('--blue'));
-}
-
-// Create a function for setting a variable value
-function myFunction_set() {
-  // Set the value of variable --blue to another value (in this case "lightblue")
-  root.style.setProperty('--blue', 'lightblue');
-}
-*/
